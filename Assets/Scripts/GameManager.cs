3cf920807d862e4d058b3f160f1edce60c08dcb1
Ramon Lopez <lopezmramon@gmail.com>
2016-01-25 23:55:19 -0430

    This is the Git Tutorial Hahaha

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameState {
+
+        MainMenu,
+        OptionsMenu,
+        SinglePlayerMenu,
+
+    }
+    public GameState currentGameState;
+    Canvas MainMenuCanvas, OptionsMenuCanvas, SinglePlayerMenuCanvas;
+    
+
+	void Start () {
+        MainMenuCanvas = GameObject.Find("MainMenu").gameObject.GetComponent<Canvas>();
+        OptionsMenuCanvas = GameObject.Find("OptionsMenu").gameObject.GetComponent<Canvas>();
+        SinglePlayerMenuCanvas = GameObject.Find("SinglePlayerMenu").gameObject.GetComponent<Canvas>();
+        ChangeState(GameState.MainMenu);
+	}
+	
+	void Update () {
+	
+	}
+
+    public void ChangeState(GameState newState)
+    {
+        currentGameState = newState;
+        StartCoroutine(newState.ToString() + "State");
+
+    }
+
+    //Game States
+
+    IEnumerator MainMenuState()
+    {
+        MainMenuCanvas.enabled = true;
+        while (currentGameState == GameState.MainMenu)
+        {
+            yield return null;
+
+        }
+        MainMenuCanvas.enabled = false;
+    }
+    IEnumerator OptionsMenuState()
+    {
+        OptionsMenuCanvas.enabled = true;
+        while (currentGameState == GameState.OptionsMenu)
+        {
+            yield return null;
+
+        }
+        OptionsMenuCanvas.enabled = false;
+
+
+    }
+    IEnumerator SinglePlayerMenuState()
+    {
+        SinglePlayerMenuCanvas.enabled = true;
+        while (currentGameState == GameState.SinglePlayerMenu)
+        {
+            yield return null;
+
+        }
+        SinglePlayerMenuCanvas.enabled = false;
+
+
+    }
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+    GameManager gameManager;
+    
+    void Start()
+    {
+
+        gameManager = FindObjectOfType<GameManager>();
+
+
+    }
+    public void Options()
+    {
+
+        gameManager.ChangeState(GameManager.GameState.OptionsMenu);
+    }
+    public void SinglePlayer()
+    {
+        gameManager.ChangeState(GameManager.GameState.SinglePlayerMenu);
+
+
+    }
+    public void Quit()
+    {
+        Debug.Log("This doesn't work in the Editor. Outside of it, the game was closed");
+        Application.Quit();
+        
+    }
+}
